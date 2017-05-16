24fb001b2277b65d7f5e52644f7bec4dff8283b1
Pola <Pola.kinomoto@gmail.com>
2017-05-16 18:25:44 -0400

    Ejercicios Eventos

---
+++ javascript/main.js
@@ -0,0 +1,12 @@
+function carga() {
+    alert("La pagina se esta cargando!");
+}
+
+function mouDentro() {
+     document.getElementById(alert("Estas dentro"));
+}
+
+function mouFuera(){
+	document.getElementById(alert("Estas fuera :("));
+}
+
